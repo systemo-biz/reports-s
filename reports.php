720a2bfda4425f4928645d679832fbcee86009fa
Anatoly Yumashev <yumashev@users.noreply.github.com>
2015-06-27 23:23:34 +0500

    Update reports.php

--- reports.php
+++ reports.php
@@ -3,8 +3,10 @@
 Plugin Name: Reports for WordPress by Systemo
 Description: Add and generate reports based php and Google Charts
 Author: Systemo
-Version: 0.20150626
 Author URI: http://systemo.biz
+GitHub Plugin URI: https://github.com/systemo-biz/reports-s
+GitHub Branch: master
+Version: 20150627
 */
 
 include_once 'inc/model.php';
